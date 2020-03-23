215964146a5a43f91d4abd4f4422b2e9b5a4430f
pdc <pengdongchu@kaytion.com>
2020-03-23 13:21:48 +0800

    从暂存区提交到 git 目录

---
+++ 01-git指令.js
@@ -0,0 +1,23 @@
+// git config --list : 检查你的配置
+// git config <key>: 检查某一项设置，例如 git config user.name
+// git config <key> value: 设置某一项设置,例如 git config --global user.name "pdc"
+
+/**
+ * 获取 git 查询手册
+ */
+// git help <verb>
+// git <verb> --help
+// 例如 git help config/git config --help
+// 如果不需要全面的手册，只需要可用的选项的快速参考，可以使用 -h 选项获取简明的 'help' 输出
+// git add -h
+
+/**
+ * 获取 git 仓库
+ */
+// 1. git init
+// 2. git clone https://github.com/libgit2/libgit2
+
+/**
+ * 检查当前文件状态
+ */
+// git status
